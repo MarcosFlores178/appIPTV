aa1939059835c3af8791e43e2211bd31967df63a
MarcosFlores178 <marcosfabianflores@gmail.com>
2026-05-27 23:49:27 -0300

    prueba local

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,7 @@
 import { IPTVChannel } from '../types/iptv';
 
-const DEVELOPMENT_API_BASE_URL = 'http://localhost:4000';
-const PRODUCTION_API_BASE_URL = 'http://localhost:4000';
+const DEVELOPMENT_API_BASE_URL = 'http://192.168.18.13:4000';
+const PRODUCTION_API_BASE_URL = 'http://192.168.18.13:4000';
 
 export const API_BASE_URL = __DEV__
   ? DEVELOPMENT_API_BASE_URL
